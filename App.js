03a1322533af5b58f116286b295e47a448cf46cc
Biju K Parambil <way2biju@gmail.com>
2022-07-02 15:23:47 +0800

    Code moved codes to sepearet files

--- App.js
+++ App.js
@@ -1,61 +1,30 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { StatusBar } from "expo-status-bar";
-import React, { useEffect } from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import React from "react";
+import { StyleSheet } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { Entypo } from "@expo/vector-icons";
 import * as SQLite from "expo-sqlite";
+import NotesStack from "./NotesStack";
+import AddScreen from "./AddScreen";
 
 const db = SQLite.openDatabase("notes.db");
-
-
-function NotesScreen({ navigation }) {
-  useEffect(() => {
-    navigation.setOptions({
-      headerRight: () => (
-        <TouchableOpacity onPress={addNote} style={styles.headerIcon}>
-          <Entypo name="new-message" size={24} color="white" />
-        </TouchableOpacity>
-      ),
-    });
-  });
-
-  function addNote() {
-    console.log("Add Note");
-  }
-
-
-  return <View style={styles.container}></View>;
-}
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator mode="modal" headerMode="none">
         <Stack.Screen
-          name="Notes"
-          component={NotesScreen}
-          options={{
-            headerTitle: "Notes App",
-            headerTitleStyle: {
-              fontWeight: "bold",
-              color: "#fff",
-              fontSize: 30,
-            },
-            headerStyle: {
-              height: 120,
-              backgroundColor: "#673ab7",
-              borderBottomColor: "#ccc",
-              borderBottomWidth: 1,
-            },
-          }}
+          name="Notes Stack"
+          component={NotesStack}
+          options={{ headerShown: false }}
         />
+        <Stack.Screen name="Add Note" component={AddScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
