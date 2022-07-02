77a9641320ac71d72558ff8212df3e34defc1aef
Biju K Parambil <way2biju@gmail.com>
2022-07-02 14:30:37 +0800

    Changed background colors

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { createStackNavigator, HeaderTitle } from "@react-navigation/stack";
+
+function NotesScreen({ navigation }) {
+  return <View style={styles.container}></View>;
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Notes"
+          component={NotesScreen}
+          options={{
+            headerTitle: "Notes App",
+            headerTitleStyle: {
+              fontWeight: "bold",
+              textAlign: "center",
+              fontSize: 30,
+              color: '#fff',
+            },
+            headerStyle: {
+              height: 120,
+              backgroundColor: "#673ab7",
+              borderBottomColor: "#ccc",
+              borderBottomWidth: 1,
+            },
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fbfbfb",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+
